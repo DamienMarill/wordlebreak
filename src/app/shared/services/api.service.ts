2c7ebcd5cdeb8c476c21d036fed95915d3db2567
Damien Marill <dev@marill.dev>
2023-02-12 23:52:15 +0100

    test

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {API_URL} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public requestApi(action: string, method: string = 'GET', datas: {} = {}, httpOptions: any = {}): Promise<any>{
+    const methodWanted = method.toLowerCase();
+    const route = API_URL + action;
+
+    let req = null;
+
+    if (httpOptions.headers === undefined){
+      httpOptions.headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      });
+    }
+
+    switch (methodWanted) {
+      case 'post' :
+        req = this.http.post(route, datas, httpOptions);
+        break;
+      case 'patch' :
+        req = this.http.post(route, datas, httpOptions);
+        break;
+      case 'put' :
+        req = this.http.put(route, datas, httpOptions);
+        break;
+      case 'delete' :
+        req = this.http.delete(route, httpOptions);
+        break;
+      default:
+        req = this.http.get(route, httpOptions);
+        break;
+    }
+
+    return req.toPromise();
+  }
 }
